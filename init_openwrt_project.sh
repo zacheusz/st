9f2148aac15c8280435f60455bfcc295589966a6
zacheusz <zacheusz@users.noreply.github.com>
2016-08-20 12:21:03 +0200

    reset to hash inside git dir

--- init_openwrt_project.sh
+++ init_openwrt_project.sh
@@ -7,8 +7,8 @@ if [ $# -eq 0 ]
 fi
 echo "cloning openwrt into $1"
 git clone git://git.openwrt.org/15.05/openwrt.git $1
-git reset --hard 17185dc391d3dc67ebd574827aeca32181c46257
 cd $1
+git reset --hard 17185dc391d3dc67ebd574827aeca32181c46257
 echo "preparing feeds.conf"
 cp feeds.conf.default feeds.conf
 echo src-git linkit https://github.com/MediaTek-Labs/linkit-smart-7688-feed.git >> feeds.conf
